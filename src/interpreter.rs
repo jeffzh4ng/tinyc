3b9fdf80aeed45c2e32e472c98be54e4da848e48
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-02-01 15:24:58 -0500

    Add if statements to ast and interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,25 +2,30 @@ use crate::ir::{Expr, Op};
 
 pub struct Interpreter {}
 
-// implicit for now:
-// - left to right order
-// - inheriting host language (rust) number semantics
 impl Interpreter {
     pub fn eval(e: Expr) -> i128 {
         match e {
-            Expr::LiteralNum(n) => n,
+            Expr::LiteralNum(n) => n, // TODO: inheriting host lang's number semantics
             Expr::Binary { op, l, r } => match op {
                 Op::Add => Interpreter::eval(*l) + Interpreter::eval(*r),
                 Op::Subtract => Interpreter::eval(*l) - Interpreter::eval(*r),
                 Op::Multiply => Interpreter::eval(*l) * Interpreter::eval(*r),
                 Op::Divide => Interpreter::eval(*l) / Interpreter::eval(*r),
             },
+            Expr::If { cond, then, els } => {
+                // TODO: 0 is the only truthy val for now
+                if Interpreter::eval(*cond) == 0 {
+                    Interpreter::eval(*then)
+                } else {
+                    Interpreter::eval(*els)
+                }
+            }
         }
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod literal_tests {
     use super::*;
 
     #[test]
@@ -29,6 +34,11 @@ mod tests {
         let output = Interpreter::eval(input);
         assert_eq!(output, 8);
     }
+}
+
+#[cfg(test)]
+mod binary_tests {
+    use super::*;
 
     #[test]
     fn binary_simple() {
@@ -72,5 +82,50 @@ mod tests {
 
         let output = Interpreter::eval(input);
         assert_eq!(output, 15);
+    }
+}
+
+#[cfg(test)]
+mod if_tests {
+    use super::*;
+
+    #[test]
+    fn if_then_simple() {
+        let input = Expr::If {
+            cond: Box::new(Expr::LiteralNum(0)),
+            then: Box::new(Expr::LiteralNum(8)),
+            els: Box::new(Expr::LiteralNum(88)),
+        };
+
+        let output = Interpreter::eval(input);
+        assert_eq!(output, 8);
+    }
+
+    #[test]
+    fn if_then_complex() {
+        let input = Expr::If {
+            cond: Box::new(Expr::Binary {
+                op: Op::Add,
+                l: Box::new(Expr::LiteralNum(10)),
+                r: Box::new(Expr::LiteralNum(-10)),
+            }),
+            then: Box::new(Expr::LiteralNum(8)),
+            els: Box::new(Expr::LiteralNum(88)),
+        };
+
+        let output = Interpreter::eval(input);
+        assert_eq!(output, 8);
+    }
+
+    #[test]
+    fn if_else() {
+        let input = Expr::If {
+            cond: Box::new(Expr::LiteralNum(1)),
+            then: Box::new(Expr::LiteralNum(8)),
+            els: Box::new(Expr::LiteralNum(88)),
+        };
+
+        let output = Interpreter::eval(input);
+        assert_eq!(output, 88);
     }
 }
--- src/ir.rs
+++ src/ir.rs
@@ -1,6 +1,15 @@
 pub enum Expr {
     LiteralNum(i128),
-    Binary { op: Op, l: Box<Expr>, r: Box<Expr> },
+    Binary {
+        op: Op,
+        l: Box<Expr>,
+        r: Box<Expr>,
+    },
+    If {
+        cond: Box<Expr>,
+        then: Box<Expr>,
+        els: Box<Expr>,
+    },
 }
 
 pub enum Op {
